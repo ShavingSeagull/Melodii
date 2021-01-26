64d458653208851c8ec16fe696266c378a2309fe
Scott Kipp <scott.ecfx@gmail.com>
2021-01-26 17:51:09 +0000

    Added SongFragment to MenuAdapter as a placeholder

--- app/src/main/java/com/example/melodii/MenuAdapter.kt
+++ app/src/main/java/com/example/melodii/MenuAdapter.kt
@@ -10,29 +10,29 @@ class MenuAdapter(context: Context, fm: FragmentManager) : FragmentPagerAdapter(
     private val mContext = context
 
     override fun getItem(position: Int): Fragment {
-        when (position) {
-            0 -> return SongFragment()
-            //1 -> return ArtistsFragment()
-            //2 -> return GenreFragment()
+        return when (position) {
+            0 -> SongFragment()
+            1 -> SongFragment()
+            2 -> SongFragment()
+            3 -> SongFragment()
             // May add playlists in as a fragment later
             // Sets Songs as the default because it is the first tab. Should never hit the default!
-            else -> return SongFragment()
+            else -> SongFragment()
         }
     }
 
     override fun getCount(): Int {
-        return 1
+        return 4
     }
 
-    // Likely won't need this!
-//    override fun getPageTitle(position: Int): CharSequence? {
-//        when (position) {
-//            0 -> return mContext.getString(R.string.continent_americas)
-//            1 -> return mContext.getString(R.string.continent_europe)
-//            2 -> return mContext.getString(R.string.continent_asia_oceania)
-//            3 -> return mContext.getString(R.string.continent_africa)
-//            // Sets Americas as the default because it is the first tab. Should never hit the default!
-//            else -> return mContext.getString(R.string.continent_americas)
-//        }
-//    }
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position) {
+            0 -> return mContext.getString(R.string.artist_tab)
+            1 -> return mContext.getString(R.string.song_tab)
+            2 -> return mContext.getString(R.string.album_tab)
+            3 -> return mContext.getString(R.string.genre_tab)
+            // Sets Americas as the default because it is the first tab. Should never hit the default!
+            else -> return mContext.getString(R.string.song_tab)
+        }
+    }
 }
