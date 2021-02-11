0d89c451099732617eda367dfc3ab1c70c7cda24
Scott Kipp <scott.ecfx@gmail.com>
2021-02-11 19:21:59 +0000

    Successfully listed songs from device into recyclerview

--- app/src/main/java/com/example/melodii/SongListFragment.kt
+++ app/src/main/java/com/example/melodii/SongListFragment.kt
@@ -11,17 +11,21 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 
 /**
  * A fragment representing a list of Items.
  */
 class SongListFragment : Fragment() {
 
+    // Data class for each song in the recyclerview
     data class Song(
         val uri: Uri,
-        val name: String
+        val songName: String,
+        val artist: String
     )
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_song_list, container, false)
@@ -41,9 +45,10 @@ class SongListFragment : Fragment() {
 
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.DISPLAY_NAME
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST
         )
-        val sortOrder = "${MediaStore.Audio.Media.DISPLAY_NAME} ASC"
+        val sortOrder = "${MediaStore.Audio.Media.TITLE} ASC"
 
         val songs = requireActivity().contentResolver.query(
             collection,
@@ -54,25 +59,26 @@ class SongListFragment : Fragment() {
         )
         songs?.use { cursor ->
             val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
-            val nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+            val nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
 
             while (cursor.moveToNext()) {
                 val id = cursor.getLong(idColumn)
                 val name = cursor.getString(nameColumn)
+                val artist = cursor.getString(artistColumn)
 
                 val contentUri: Uri = ContentUris.withAppendedId(
                     MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
                     id
                 )
 
-                songClassList += Song(contentUri, name)
+                songClassList += Song(contentUri, name, artist)
             }
         }
 
-        // Dummy data list
-        val dummyList = generateDummyList(100)
-        // Actual list of songs on device
+        // List of songs on device
         val songList = addSongsToList(songClassList)
+
         // Set the adapter
         if (view is RecyclerView) {
             with(view) {
@@ -83,23 +89,13 @@ class SongListFragment : Fragment() {
         return view
     }
 
-    private fun generateDummyList(size: Int): List<CardItem> {
-        val list = ArrayList<CardItem>()
-
-        for (i in 0 until size) {
-            val drawable = R.drawable.ic_baseline_music_note_24
-            val item = CardItem(drawable, "Song $i", "Subtitle")
-            list += item
-        }
-        return list
-    }
-
+    // Adds songs from the device of the Song class into a list of CardItems for the recyclerview
     private fun addSongsToList(songList: MutableList<Song>): List<CardItem> {
         val list = ArrayList<CardItem>()
 
         for (i in songList) {
             val drawable = R.drawable.ic_baseline_music_note_24
-            val item = CardItem(drawable, "$i", "Subtitle")
+            val item = CardItem(drawable, i.songName, i.artist)
             list += item
         }
         return list
